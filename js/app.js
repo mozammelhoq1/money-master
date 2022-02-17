c2f33765055ad3507ae6e9ee048f5b2afb076057
mozammelhoq1 <mozammelhoq512@gmail.com>
2022-02-17 23:19:52 +0600

    update money master

--- js/app.js
+++ js/app.js
@@ -1,61 +1,3 @@
-//calculate button
-document.getElementById('calculate-button').addEventListener('click', function () {
-  // total expenses
-  const totalExpenses = document.getElementById('total-expenses');
-  const totalExpensesNumber = getInputValue('food-input') + getInputValue('rent-input') + getInputValue('clothes-input');
-  //balance 
-  const balanceAmount = document.getElementById('balance');
-  //income amount
-  const incomeAmount = getInputValue('income-input');
-  //error check
-  if (totalExpensesNumber >= 0 && incomeAmount >= 0) {
-      totalExpenses.innerText = totalExpensesNumber;
-      //error check
-      if (totalExpensesNumber <= incomeAmount) {
-          balanceAmount.innerText = incomeAmount - getTextValue('total-expenses');
-          mathError('total-expenses','expenses',false);
-          mathError('balance','expenses', false);
-      }
-      else {
-          balanceAmount.innerText = '00';
-          mathError('total-expenses','expenses',true);
-      }
-  }
-  else {
-      totalExpenses.innerText = '00';
-      balanceAmount.innerText = '00';
-  }
-});
-
-//save button 
-document.getElementById('save-button').addEventListener('click', function () {
-  // save amount
-  const saveAmount = document.getElementById('save-amount');
-  const saveAmountNumber = getInputValue('income-input') * (getInputValue('save-input') / 100);
-    // remain amount 
-  const remainAmount = document.getElementById('remain-amount');
-  //error check
-  if (saveAmountNumber >= 0) {
-      saveAmount.innerText = saveAmountNumber;
-      //error check
-      if (getTextValue('save-amount') <= getTextValue('balance')) {
-          remainAmount.innerText = getTextValue('balance') - getTextValue('save-amount');
-          mathError('save-amount','saving',false);
-          mathError('balance', 'saving', false);
-      }
-      else {
-          remainAmount.innerText = '00';
-          mathError('save-amount', 'saving', true);
-          mathError('balance', 'saving', true);
-      }
-  }
-  else if(saveAmountNumber.typeof != 'number'){
-      saveAmount.innerText = '00';
-      remainAmount.innerText = '00';
-  }
-});
-
-
 // check error display
 function errorDisplay(amount, displayId, border, isError) {
   if (isError == true) {
@@ -135,6 +77,66 @@ function getTextValue(textId) {
   const textValue = parseFloat(text.innerText);
   return textValue;
 };
+
+//calculate button
+document.getElementById('calculate-button').addEventListener('click', function () {
+  // total expenses
+  const totalExpenses = document.getElementById('total-expenses');
+  const totalExpensesNumber = getInputValue('food-input') + getInputValue('rent-input') + getInputValue('clothes-input');
+  //balance 
+  const balanceAmount = document.getElementById('balance');
+  //income amount
+  const incomeAmount = getInputValue('income-input');
+  //error check
+  if (totalExpensesNumber >= 0 && incomeAmount >= 0) {
+      totalExpenses.innerText = totalExpensesNumber;
+      //error check
+      if (totalExpensesNumber <= incomeAmount) {
+          balanceAmount.innerText = incomeAmount - getTextValue('total-expenses');
+          mathError('total-expenses','expenses',false);
+          mathError('balance','expenses', false);
+      }
+      else {
+          balanceAmount.innerText = '00';
+          mathError('total-expenses','expenses',true);
+      }
+  }
+  else {
+      totalExpenses.innerText = '00';
+      balanceAmount.innerText = '00';
+  }
+});
+
+//save button 
+document.getElementById('save-button').addEventListener('click', function () {
+  // save amount
+  const saveAmount = document.getElementById('save-amount');
+  const saveAmountNumber = getInputValue('income-input') * (getInputValue('save-input') / 100);
+    // remain amount 
+  const remainAmount = document.getElementById('remain-amount');
+  //error check
+  if (saveAmountNumber >= 0) {
+      saveAmount.innerText = saveAmountNumber;
+      //error check
+      if (getTextValue('save-amount') <= getTextValue('balance')) {
+          remainAmount.innerText = getTextValue('balance') - getTextValue('save-amount');
+          mathError('save-amount','saving',false);
+          mathError('balance', 'saving', false);
+      }
+      else {
+          remainAmount.innerText = '00';
+          mathError('save-amount', 'saving', true);
+          mathError('balance', 'saving', true);
+      }
+  }
+  else if(saveAmountNumber.typeof != 'number'){
+      saveAmount.innerText = '00';
+      remainAmount.innerText = '00';
+  }
+});
+
+
+
 
 
 
